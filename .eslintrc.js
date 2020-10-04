1f3867a0e7527d55db4f896d8db8f601763f8f64
nickzylstra <nick.zylstra@gmail.com>
2020-10-04 12:46:38 -0700

    Add eslint airbnb config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:jest/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    // not supported yet in Airbnb
+    "@typescript-eslint/camelcase": "off",
+  },
+};
