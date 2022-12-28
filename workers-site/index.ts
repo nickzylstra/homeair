70021c93a982d2d13568f807adf7f56669420219
Nick Zylstra <nick.zylstra@gmail.com>
2022-12-27 18:20:30 -0800

    Setup for retrieving purple air data from new api and caching with kv

--- workers-site/index.ts
+++ workers-site/index.ts
@@ -1,5 +1,4 @@
 import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
-import type { Options } from '@cloudflare/kv-asset-handler';
 //@ts-ignore
 import manifestJSON from '__STATIC_CONTENT_MANIFEST';
 const assetManifest = JSON.parse(manifestJSON);
@@ -17,6 +16,7 @@ export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
   // MY_KV_NAMESPACE: KVNamespace;
   __STATIC_CONTENT: KVNamespace;
+  API_CACHE: KVNamespace;
   //
   // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
   // MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -26,11 +26,7 @@ export interface Env {
 }
 
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext,
-  ): Promise<Response> {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     try {
       const url = new URL(request.url);
       if (url.pathname.startsWith('/api')) {
@@ -48,19 +44,35 @@ export default {
   },
 };
 
-async function handleAPIRequest(
-  request: Request,
-  env: Env,
-  ctx: ExecutionContext,
-) {
-  return new Response('hello API');
+async function handleAPIRequest(request: Request, env: Env, ctx: ExecutionContext) {
+  const url = new URL(request.url);
+
+  const sensorDataKey = 'sensor-data';
+
+  if (!url.pathname.startsWith(`/api/${sensorDataKey}`))
+    return new Response(`No API endpoint found for ${url.pathname}`, {
+      status: 404,
+    });
+
+  // get data from kv if exists (not expired)
+  const sensorData = await env.API_CACHE.get(sensorDataKey);
+  if (sensorData !== null) {
+    return new Response(sensorData, {
+      headers: { 'content-type': 'application/json' },
+    });
+  }
+
+  console.log(`api kv cache miss for ${sensorDataKey}`);
+  // TODO get data from purple air using API key stored as env var
+  const purpleAirData = JSON.stringify({ data: 'pa-data' });
+  await env.API_CACHE.put(sensorDataKey, purpleAirData, {
+    expirationTtl: 60,
+  });
+
+  return new Response(purpleAirData, { headers: { 'content-type': 'application/json' } });
 }
 
-async function handleAssetRequest(
-  request: Request,
-  env: Env,
-  ctx: ExecutionContext,
-) {
+async function handleAssetRequest(request: Request, env: Env, ctx: ExecutionContext) {
   const url = new URL(request.url);
   try {
     return await getAssetFromKV(
@@ -90,8 +102,7 @@ async function handleAssetRequest(
             },
           },
           {
-            mapRequestToAsset: (req) =>
-              new Request(`${new URL(req.url).origin}/404.html`, req),
+            mapRequestToAsset: (req) => new Request(`${new URL(req.url).origin}/404.html`, req),
           },
         );
 
