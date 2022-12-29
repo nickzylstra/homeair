64a32885bdd71cfe7e2c4ee67dc3375f089dff98
Nick Zylstra <nick.zylstra@gmail.com>
2022-12-29 14:50:24 -0800

    Show chart data with 15 minute averages

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -12,10 +12,59 @@ import {
 } from 'recharts';
 import { ChartDataPoint } from './types';
 
+function averageArr(arr: number[]): number | undefined {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  return Math.round(arr.reduce((a, b) => a + b) / arr.length);
+}
+
+function averageData(data: ChartDataPoint[]): ChartDataPoint[] {
+  const averageChartDataMinutes = 15;
+  const averagedData: ChartDataPoint[] = [];
+
+  let tempPoints: ChartDataPoint[] = [];
+  data.forEach((p, i) => {
+    tempPoints.push(p);
+
+    if ((i + 1) % averageChartDataMinutes === 0 || i === data.length - 1) {
+      const atp = tempPoints[tempPoints.length - 1];
+      const intervalPoints: Partial<Record<keyof ChartDataPoint, number[]>> = {
+        ourHouseTempF: [],
+        ourHouseRelHumidityPerc: [],
+        ourHouseAQI: [],
+        outside1AQI: [],
+        outside2AQI: [],
+        outsideAvgAQI: [],
+      };
+      tempPoints.forEach((tp) => {
+        Object.entries(tp).forEach(([k, v]) => {
+          if (typeof v === 'number' && v !== undefined) {
+            intervalPoints[k as keyof ChartDataPoint]?.push(v);
+          }
+        });
+      });
+
+      atp.ourHouseTempF = averageArr(intervalPoints.ourHouseTempF ?? []);
+      atp.ourHouseRelHumidityPerc = averageArr(intervalPoints.ourHouseRelHumidityPerc ?? []);
+      atp.ourHouseAQI = averageArr(intervalPoints.ourHouseAQI ?? []);
+      atp.outside1AQI = averageArr(intervalPoints.outside1AQI ?? []);
+      atp.outside2AQI = averageArr(intervalPoints.outside2AQI ?? []);
+      atp.outsideAvgAQI = averageArr(intervalPoints.outsideAvgAQI ?? []);
+
+      averagedData.push(atp);
+      tempPoints = [];
+    }
+  });
+
+  return averagedData;
+}
+
 function Chart({ data }: { data: ChartDataPoint[] }) {
+  const averagedData = averageData(data);
   return (
     <ResponsiveContainer width="95%" height={600}>
-      <LineChart data={data}>
+      <LineChart data={averagedData}>
         <Line type="monotone" dataKey="ourHouseAQI" stroke="#8884d8" />
         <Line type="monotone" dataKey="outsideAvgAQI" stroke="#1884d8" />
         <Line type="monotone" dataKey="ourHouseTempF" stroke="#50C878" />
